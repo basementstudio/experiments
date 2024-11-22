7d8b9edf3307667e01964b42a0f0b3d6d1bbcb6d
Vittorio Retrivi <vittorioretrivi@gmail.com>
2024-11-22 12:30:24 -0300

    warp

--- src/experiments/78.crt-renderer/shaders/shaders.ts
+++ src/experiments/78.crt-renderer/shaders/shaders.ts
@@ -1,12 +1,20 @@
 export const fragment = /* glsl */ `
-  void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-    // Just output red to test if the effect is working
-    outputColor = vec4(1.0, 0.0, 0.0, 1.0);
-  }
-`
+  const float warp = 0.75;
 
-export const vertex = /* glsl */ `
-  void mainSupport(const in vec2 uv) {
-    // Empty but required
-  }
+  void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
+    
+
+    // warp the uv
+    // vec2 dc = abs(0.5-uv);
+    // dc *= dc;
+    // vec2 warpedUv = uv;
+    // warpedUv.x -= 0.5; warpedUv.x *= 1.0+(dc.y*(0.3*warp)); warpedUv.x += 0.5;
+    // warpedUv.y -= 0.5; warpedUv.y *= 1.0+(dc.x*(0.4*warp)); warpedUv.y += 0.5;
+    // if (warpedUv.y > 1.0 || warpedUv.x < 0.0 || warpedUv.x > 1.0 || warpedUv.y < 0.0) {
+    //     outputColor = vec4(0.0,0.0,0.0,1.0);
+    //     return;
+    // }
+
+  outputColor = inputColor;
+}
 `
