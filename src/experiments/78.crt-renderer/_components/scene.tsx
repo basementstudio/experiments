1f4c6080b451c20efbbdaaaf5dab518da0d0c8f9
Vittorio Retrivi <vittorioretrivi@gmail.com>
2024-11-22 13:04:01 -0300

    some effects

--- src/experiments/78.crt-renderer/_components/scene.tsx
+++ src/experiments/78.crt-renderer/_components/scene.tsx
@@ -1,14 +1,26 @@
 import { OrbitControls } from '@react-three/drei'
-import { EffectComposer, wrapEffect } from '@react-three/postprocessing'
+import { Bloom, EffectComposer, wrapEffect } from '@react-three/postprocessing'
 import { Effect } from 'postprocessing'
+import { Uniform } from 'three'
 
 import { fragment } from '../shaders/shaders'
 
 class CrtEffectImpl extends Effect {
   constructor() {
     super('CrtEffect', fragment, {
-      uniforms: new Map([])
+      uniforms: new Map([
+        ['uTime', new Uniform(0)],
+        ['uNoiseIntensity', new Uniform(0.15)],
+        ['uWarpStrength', new Uniform(0.75)],
+        ['uScanlineIntensity', new Uniform(0.1)],
+        ['uScanlineFrequency', new Uniform(128.0)]
+      ])
     })
+  }
+
+  update(renderer: any, inputBuffer: any, deltaTime: number) {
+    // @ts-expect-error
+    this.uniforms.get('uTime').value += deltaTime
   }
 }
 
@@ -19,12 +31,33 @@ export function Scene() {
     <>
       <OrbitControls />
 
-      <mesh receiveShadow castShadow scale={2}>
-        <sphereGeometry args={[1, 64, 64]} />
-        <meshStandardMaterial color="skyblue" />
-      </mesh>
+      <group>
+        <mesh receiveShadow castShadow position={[-2, 0, 0]}>
+          <torusKnotGeometry args={[1, 0.3, 128, 32]} />
+          <meshStandardMaterial
+            color="hotpink"
+            roughness={0.3}
+            metalness={0.7}
+          />
+        </mesh>
+
+        <mesh receiveShadow castShadow position={[2, 0, 0]}>
+          <icosahedronGeometry args={[1, 1]} />
+          <meshStandardMaterial color="lime" roughness={0.2} metalness={0.8} />
+        </mesh>
+
+        <mesh receiveShadow castShadow position={[0, 2, 0]}>
+          <octahedronGeometry args={[1]} />
+          <meshStandardMaterial
+            color="orange"
+            roughness={0.4}
+            metalness={0.6}
+          />
+        </mesh>
+      </group>
 
       <EffectComposer>
+        <Bloom mipmapBlur intensity={1.5} luminanceThreshold={0.8} />
         {/* @ts-expect-error */}
         <CrtEffect />
       </EffectComposer>
--- src/experiments/78.crt-renderer/shaders/shaders.ts
+++ src/experiments/78.crt-renderer/shaders/shaders.ts
@@ -1,20 +1,41 @@
 export const fragment = /* glsl */ `
-  const float warp = 0.75;
+    uniform float uTime;
 
-  void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-    
+    uniform float uNoiseIntensity;
 
-    // warp the uv
-    // vec2 dc = abs(0.5-uv);
-    // dc *= dc;
-    // vec2 warpedUv = uv;
-    // warpedUv.x -= 0.5; warpedUv.x *= 1.0+(dc.y*(0.3*warp)); warpedUv.x += 0.5;
-    // warpedUv.y -= 0.5; warpedUv.y *= 1.0+(dc.x*(0.4*warp)); warpedUv.y += 0.5;
-    // if (warpedUv.y > 1.0 || warpedUv.x < 0.0 || warpedUv.x > 1.0 || warpedUv.y < 0.0) {
-    //     outputColor = vec4(0.0,0.0,0.0,1.0);
-    //     return;
-    // }
+    uniform float uWarpStrength;
 
-  outputColor = inputColor;
-}
+    uniform float uScanlineIntensity;
+    uniform float uScanlineFrequency;
+
+
+    // Add noise function
+    float random(vec2 st) {
+        return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+    }
+
+    void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
+
+        // warp the uv
+        vec2 dc = abs(0.5-uv);
+        dc *= dc;
+        vec2 warpedUv = uv;
+        warpedUv.x -= 0.5; warpedUv.x *= 1.0+(dc.y*(0.3*uWarpStrength)); warpedUv.x += 0.5;
+        warpedUv.y -= 0.5; warpedUv.y *= 1.0+(dc.x*(0.4*uWarpStrength)); warpedUv.y += 0.5;
+        if (warpedUv.y > 1.0 || warpedUv.x < 0.0 || warpedUv.x > 1.0 || warpedUv.y < 0.0) {
+            outputColor = vec4(0.0,0.0,0.0,1.0);
+            return;
+        }
+
+        // Add scanlines
+        float scanLine = sin(warpedUv.y * uScanlineFrequency) * uScanlineIntensity;
+        vec4 color = inputColor;
+        color.rgb -= scanLine;
+
+        // Add noise
+        float noise = random(uv + uTime) * uNoiseIntensity;
+        color.rgb += noise;
+
+        outputColor = color;
+    }
 `
